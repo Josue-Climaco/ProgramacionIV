756b07e7a15ef4afcb481baf2d15fec13a759ba0
Josue-Climaco <hjosue656@gmail.com>
2020-03-05 16:01:09 -0600

    parcial

--- app.js
+++ app.js
@@ -1,37 +1,83 @@
 
 document.addEventListener("DOMContentLoaded", e=>{
-    document.querySelector("#frmAlumnos").addEventListener("submit", event=>{
+    const form = document.querySelector("#frmConversores");
+    form.addEventListener("submit", event=>{
         event.preventDefault();
+  
+        let de = document.querySelector("#cboDe").value,
+            a = document.querySelector("#cboA").value,
+            cantidad = document.querySelector("#txtCantidadConversor").value,
+            opcion = document.getElementById('cboConvertir');
+  
+        let monedas = {
+            "dolar":1,
+            "Colones (SV)":8.75,
+            "Yenes":111.27,
+            "Rupia":69.75,
+            "lempira":24.9,
+            "Peso (MX) ":19.36,
+            "Bitcoin":0.00026},
 
-        let codigo = document.querySelector("#txtCodigoAlumno").value,
-            nombre = document.querySelector("#txtNombreAlumno").value,
-            direccion = document.querySelector("#txtDireccionAlumno").value,
-            telefono = document.querySelector("#txtTelefonoAlumno").value;
 
-        console.log(codigo, nombre, direccion, telefono);
-        
-        if( 'localStorage' in window ){
-            window.localStorage.setItem("codigo", codigo);
-            window.localStorage.setItem("nombre", nombre);
-            window.localStorage.setItem("direccion", direccion);
-            window.localStorage.setItem("telefono", telefono);
-        } else {
-            alert("Por favor ACTUALIZATE!!!.");
-        }
-    });
-    document.querySelector("#btnRecuperarAlumno").addEventListener("click", event=>{
-        document.querySelector("#txtCodigoAlumno").value = window.localStorage.getItem("codigo");
-        document.querySelector("#txtNombreAlumno").value = window.localStorage.getItem("nombre");
-        document.querySelector("#txtDireccionAlumno").value = window.localStorage.getItem("direccion");
-        document.querySelector("#txtTelefonoAlumno").value = window.localStorage.getItem("telefono");
-    });
-});
+            almacenamiento = {
+            "megabyte":1,
+            "bit": 8388608,
+            "byte": 1048576,
+            "kilobyte": 1024,
+            "gigabyte":0.000000001,
+            "Terabyte":0.00000095367431660625},
 
-/*document.addEventListener("DOMContentLoaded",function(e){
-    alert("CALLBACK LISTO");
-});*/
+            longitud = {
+            "Metro": 1,
+            "cm": 100,
+            "Pulgada": 39.3701,
+            "Pie":3.28084,
+            "Varas":1.1963081929167,
+            "Yardas":1.09361,
+            "Km": 0.001,
+            "milla": 0.000621371};
 
-/*document.addEventListener("DOMContentLoaded", init);
-function init(e){
-    alert("LISTO");
-}*/
+            let $res = document.querySelector("#lblRespuesta");
+            if(opcion.value == "moneda"){
+            $res.innerHTML = `Respuesta: ${ (monedas[a]/monedas[de]*cantidad).toFixed(2) }`;
+            } else if(opcion.value == "almacenamiento"){
+            $res.innerHTML = `Respuesta: ${ (almacenamiento[a]/almacenamiento[de]*cantidad) }`;
+            } else if(opcion.value == "longitud"){
+            $res.innerHTML = `Respuesta: ${ (longitud[a]/longitud[de]*cantidad).toFixed(2) }`;
+            };
+        });
+      });
+      
+      function seleccionar() {
+        let opcion = document.getElementById('cboConvertir'),
+        de1 = document.getElementById('cboDe'),
+        a1 = document.getElementById('cboA');
+        de1.value="";
+        a1.value="";
+        de1.innerHTML="";
+        a1.innerHTML="";
+      
+        if(opcion.value == "moneda"){
+          var  array = ["dolar!Dolar","colones (sv)!Colones(SV) ","yenes!Yenes","rupia!Rupia","lempiras!Lempiras"," peso (mx)! Peso (MX)","bitcoin!Bitcoin"];
+        } else if(opcion.value == "almacenamiento"){
+            var array = ["megabyte!Megabyte","bit!Bit","byte!Byte","kilobyte!Kilobyte","terabyte!Terabyte","gigabyte!Gigabyte"]; 
+        } else if(opcion.value == "longitud"){
+          var array = ["metros!Metros","cm!Centimetros","pulgada!Pulgada","pie!Pie","varas!Varas","yardas!Yardas","km!Kilometros","millas!Millas"];
+        };
+      
+        for(var i=0;i<array.length;i++){ 
+          var repair = array[i].split("!");
+          var newop = document.createElement("option");
+          newop.value = repair[0]
+          newop.innerHTML = repair[1];
+          de1.options.add(newop);
+        };
+        for(var i=0;i<array.length;i++){ 
+          var repair = array[i].split("!");
+          var newop = document.createElement("option");
+          newop.value = repair[0]
+          newop.innerHTML = repair[1];
+          a1.options.add(newop);
+        };
+       }
+    
